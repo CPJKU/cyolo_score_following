7e2204255b4af4944957b0147dd3f06a309376f7
Florian Henkel <florian.henkel@jku.at>
2021-10-25 14:07:17 +0200

    update loss for bar/system prediction

--- cyolo_score_following/utils/general.py
+++ cyolo_score_following/utils/general.py
@@ -47,7 +47,55 @@ def xywh2xyxy(x):
     return y
 
 
-def get_max_box(prediction):
+def box_iou(box1, box2):
+    # https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
+    """
+    Return intersection-over-union (Jaccard index) of boxes.
+    Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
+    Arguments:
+        box1 (Tensor[N, 4])
+        box2 (Tensor[M, 4])
+    Returns:
+        iou (Tensor[N, M]): the NxM matrix containing the pairwise
+            IoU values for every element in boxes1 and boxes2
+    """
+
+    def box_area(box):
+        # box = 4xn
+        return (box[2] - box[0]) * (box[3] - box[1])
+
+    area1 = box_area(box1.T)
+    area2 = box_area(box2.T)
+
+    # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
+    inter = (torch.min(box1[:, None, 2:], box2[:, 2:]) - torch.max(box1[:, None, :2], box2[:, :2])).clamp(0).prod(2)
+    return inter / (area1[:, None] + area2 - inter)  # iou = inter / (area1 + area2 - inter)
+
+
+def bbox_iou(box1, box2, eps=1e-9):
+    # Returns the IoU of box1 to box2. box1 is 4xn, box2 is nx4
+    box2 = box2.T
+
+    # transform from xywh to xyxy
+    b1_x1, b1_x2 = box1[0] - box1[2] / 2, box1[0] + box1[2] / 2
+    b1_y1, b1_y2 = box1[1] - box1[3] / 2, box1[1] + box1[3] / 2
+    b2_x1, b2_x2 = box2[0] - box2[2] / 2, box2[0] + box2[2] / 2
+    b2_y1, b2_y2 = box2[1] - box2[3] / 2, box2[1] + box2[3] / 2
+
+    # Intersection area
+    inter = (torch.min(b1_x2, b2_x2) - torch.max(b1_x1, b2_x1)).clamp(0) * \
+            (torch.min(b1_y2, b2_y2) - torch.max(b1_y1, b2_y1)).clamp(0)
+
+    # Union Area
+    w1, h1 = b1_x2 - b1_x1, b1_y2 - b1_y1 + eps
+    w2, h2 = b2_x2 - b2_x1, b2_y2 - b2_y1 + eps
+    union = w1 * h1 + w2 * h2 - inter + eps
+
+    iou = inter / union
+    return iou
+
+
+def get_max_box(prediction, class_id=0):
     """
     Returns:
          most confident detection with shape: 1x4 (x, y, w, h)
@@ -55,13 +103,16 @@ def get_max_box(prediction):
 
     output = []
     for xi, x in enumerate(prediction):  # image index, image inference
+        class_filtered = x[:, -1] == class_id
+
+        x = x[class_filtered]  # confidence
+
         _, idx = x[..., 4].max(-1)
         max_per_sample = x[idx][:4]
         output.append(max_per_sample)
 
     output = torch.stack(output)
 
-    assert output.shape[0] == prediction.shape[0]
     return output
 
 
--- cyolo_score_following/utils/loss.py
+++ cyolo_score_following/utils/loss.py
@@ -2,7 +2,11 @@
 adapted from https://github.com/ultralytics/yolov5/blob/master/utils/loss.py
 """
 import torch
+
+import numpy as np
 import torch.nn as nn
+
+from cyolo_score_following.utils.general import bbox_iou
 
 
 def compute_loss(p, targets, model):  # predictions, targets, model
@@ -11,6 +15,20 @@ def compute_loss(p, targets, model):  # predictions, targets, model
     tbox, indices, anchors = build_targets(p, targets, model)  # targets
 
     BCEobj = nn.BCEWithLogitsLoss().to(device)
+
+    loss_dict = {}
+
+    box_balance = np.asarray([1., 1., 1.])
+
+    if model.loss_type == "mse" or model.loss_type == "mse_diff":
+        obj_balance = np.asarray([10., 1., 1.])
+        lbox_weight = 0.1
+        lobj_weight = 1.0
+    else:
+        raise NotImplementedError
+
+    box_balance *= lbox_weight
+    obj_balance *= lobj_weight
 
     # Losses
     for i, pi in enumerate(p):  # layer index, layer predictions
@@ -23,22 +41,51 @@ def compute_loss(p, targets, model):  # predictions, targets, model
 
             # Regression
             pxy = ps[:, :2].sigmoid() * 2. - 0.5
-            pwh = (ps[:, 2:4].sigmoid() * 2) ** 2 * anchors[i]
-            pbox = torch.cat((pxy, pwh), 1).to(device)  # predicted box
+            pwh = (ps[:, 2:4].sigmoid() * 2) ** 2
+            pbox = torch.cat((pxy, pwh * anchors[i]), 1).to(device)  # predicted box
 
-            iou = bbox_iou(pbox.T, tbox[i])  # iou(prediction, target)
+            if model.loss_type == "mse":
 
-            lbox += ((pbox - tbox[i])**2).mean()
+                iou = bbox_iou(pbox.T, tbox[i])  # iou(prediction, target)
+                if model.nc == 1:
+                    lbox_i = ((pbox - tbox[i])**2).mean()
+
+                else:
+                    lbox_i = ((pbox[:, :2] - tbox[i][:, :2]) ** 2 +
+                              (pbox[:, 2:].sqrt() - tbox[i][:, 2:].sqrt()) ** 2).mean()
+
+            elif model.loss_type == "mse_diff":
+                iou = bbox_iou(pbox.T, tbox[i])
+                lbox_i = ((pxy - tbox[i][:, :2]) ** 2 + (pwh - tbox[i][:, 2:]/anchors[i]) ** 2).mean()
+
+            else:
+                raise NotImplementedError
 
             # Objectness
             tobj[b, a, gj, gi] = iou.detach().clamp(0).type(tobj.dtype)  # iou ratio
+            lobj_i = BCEobj(pi[b.unique(), ..., 4], tobj[b.unique()])
 
-        lobj += BCEobj(pi[..., 4], tobj)  # obj loss
+            lbox_i *= box_balance[i]
+            lobj_i *= obj_balance[i]
 
-    lbox *= 0.1
+            lbox += lbox_i
+            lobj += lobj_i
+
+            loss_dict[f'box_loss_{i}'] = lbox_i
+            loss_dict[f'obj_loss_{i}'] = lobj_i
+
+        else:
+            # dummy loss that should have zero gradient, otherwise DistributedDataParallel
+            # can throw errors if not all parameters are used
+            lobj += (pi * 0).mean()
+
     loss = lbox + lobj
 
-    return {'loss': loss, 'box_loss': lbox, 'obj_loss': lobj}
+    loss_dict['loss'] = loss
+    loss_dict['box_loss'] = lbox
+    loss_dict['obj_loss'] = lobj
+
+    return loss_dict
 
 
 def build_targets(p, targets, model):
@@ -48,8 +95,15 @@ def build_targets(p, targets, model):
     na, nt = det.na, targets.shape[0]  # number of anchors, targets
     tbox, indices, anch = [], [], []
     gain = torch.ones(7, device=targets.device)  # normalized to gridspace gain
-    ai = torch.arange(na, device=targets.device).float().view(na, 1).repeat(1, nt)  # same as .repeat_interleave(nt)
-    targets = torch.cat((targets.repeat(na, 1, 1), ai[:, :, None]), 2)  # append anchor indices
+
+    targets_ = []
+
+    for class_idx in range(det.nc):
+        target_c = targets[targets[:, 1] == class_idx]
+        nt = target_c.shape[0]
+        ai = torch.arange(na, device=targets.device).float().view(na, 1).repeat(1, nt)
+
+        targets_.append(torch.cat((target_c.repeat(na, 1, 1), ai[:, :, None]), 2))
 
     anchor_t = 4.0  # anchor-multiple threshold
     g = 0.5  # bias
@@ -62,11 +116,12 @@ def build_targets(p, targets, model):
         gain[2:6] = torch.tensor(p[i].shape)[[3, 2, 3, 2]]  # xyxy gain
 
         # Match targets to anchors
-        t = targets * gain
-        if nt:
-            # Matches
+        t = targets_[i] * gain
+        if t.shape[1] > 0:
+            # Matching anchors
             r = t[:, :, 4:6] / anchors[:, None]  # wh ratio
             j = torch.max(r, 1. / r).max(2)[0] < anchor_t  # compare
+
             t = t[j]  # filter
 
             # Offsets
@@ -77,44 +132,23 @@ def build_targets(p, targets, model):
             j = torch.stack((torch.ones_like(j), j, k, l, m))
             t = t.repeat((5, 1, 1))[j]
             offsets = (torch.zeros_like(gxy)[None] + off[:, None])[j]
+
+            # Define
+            b, c = t[:, :2].long().T  # image, class
+            gxy = t[:, 2:4]  # grid xy
+            gwh = t[:, 4:6]  # grid wh
+            gij = (gxy - offsets).long()
+            gi, gj = gij.T  # grid xy indices
+
+            # Append
+            a = t[:, 6].long()  # anchor indices
+            indices.append((b, a, gj, gi))  # image, anchor, grid indices
+            tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
+            anch.append(anchors[a])  # anchors
         else:
-            t = targets[0]
-            offsets = 0
-
-        # Define
-        b, c = t[:, :2].long().T  # image, class
-        gxy = t[:, 2:4]  # grid xy
-        gwh = t[:, 4:6]  # grid wh
-        gij = (gxy - offsets).long()
-        gi, gj = gij.T  # grid xy indices
-
-        # Append
-        a = t[:, 6].long()  # anchor indices
-        indices.append((b, a, gj, gi))  # image, anchor, grid indices
-        tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
-        anch.append(anchors[a])  # anchors
+            indices.append((torch.zeros(0, dtype=torch.long), torch.zeros(0), torch.zeros(0), torch.zeros(0)))
+            tbox.append(None)
+            anch.append(None)
 
     return tbox, indices, anch
 
-
-def bbox_iou(box1, box2, eps=1e-9):
-    # Returns the IoU of box1 to box2. box1 is 4xn, box2 is nx4
-    box2 = box2.T
-
-    # transform from xywh to xyxy
-    b1_x1, b1_x2 = box1[0] - box1[2] / 2, box1[0] + box1[2] / 2
-    b1_y1, b1_y2 = box1[1] - box1[3] / 2, box1[1] + box1[3] / 2
-    b2_x1, b2_x2 = box2[0] - box2[2] / 2, box2[0] + box2[2] / 2
-    b2_y1, b2_y2 = box2[1] - box2[3] / 2, box2[1] + box2[3] / 2
-
-    # Intersection area
-    inter = (torch.min(b1_x2, b2_x2) - torch.max(b1_x1, b2_x1)).clamp(0) * \
-            (torch.min(b1_y2, b2_y2) - torch.max(b1_y1, b2_y1)).clamp(0)
-
-    # Union Area
-    w1, h1 = b1_x2 - b1_x1, b1_y2 - b1_y1 + eps
-    w2, h2 = b2_x2 - b2_x1, b2_y2 - b2_y1 + eps
-    union = w1 * h1 + w2 * h2 - inter + eps
-
-    iou = inter / union
-    return iou
